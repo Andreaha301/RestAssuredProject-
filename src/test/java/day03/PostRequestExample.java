70fb0a085f19f3a10e516ef8ce4800d8d14a2b14
Andreaha301 <andrea_halatrista@hotmail.com>
2020-08-21 16:01:25 -0400

    Adding new classes from day3 package

--- src/test/java/day03/PostRequestExample.java
+++ src/test/java/day03/PostRequestExample.java
@@ -23,6 +23,8 @@ public class PostRequestExample {
 
     }
 
+
+
     @DisplayName("Testing POST /api/spartans")
     @Test
     public void testAddSpartan(){
@@ -43,7 +45,43 @@ public class PostRequestExample {
         then()
                 .log().all()
                 .statusCode( is(201))
-                .contentType(ContentType.JSON);
+                .contentType(ContentType.JSON)
+                .body("success", is("A Spartan is Born!"))
+                .body("data.name", is("Carola Marchani"))
+
+        ;
+    }
+
+     // Please create another test
+    // make a post request , store the response Object
+    // save the id into int variable
+    // save the name into String
+    // print them out.
+    @DisplayName("Practice extracting data")
+    @Test
+    public void postRequestExtractingData(){
+
+        String myBodyData = "{\n" +
+                "  \"name\": \"Carola Marchani\",  \n" +
+                "  \"gender\": \"Male\",\n" +
+                "  \"phone\": 3018205382\n" +
+                "}" ;
+
+        Response response = given()
+                                   .contentType(ContentType.JSON)
+                                   .body(myBodyData)
+                                   .log().all().
+                            when()
+                                   .post("/spartans")
+                                   .prettyPeek() ;
+
+        System.out.println("The id is " + response.path("data.id"));
+        System.out.println("The name is " + response.path("data.name"));
+
+        JsonPath jp = response.jsonPath() ;
+        System.out.println("ID using jsonPath " + jp.getInt("data.id"));
+        System.out.println("Name using jsonPath " + jp.getString("data.name"));
+
 
 
     }
---
+++ src/test/java/day03/PutRequestExample.java
@@ -0,0 +1,62 @@
+package day03;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+
+public class PutRequestExample {
+
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI = "http://34.229.100.122" ;
+        RestAssured.port = 8000 ;
+        RestAssured.basePath = "/api" ;
+    }
+
+
+    @DisplayName("Updating existing Data")
+    @Test
+    public void updateSpartan(){
+
+        String updatedBody = "{\n" +
+                "        \"id\": 205,\n" +
+                "        \"name\": \"Wonder Woman\",\n" +
+                "        \"gender\": \"Male\",\n" +
+                "        \"phone\": 3018205382\n" +
+                "    }" ;
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedBody)
+                .log().all().
+        when()
+                .put("/spartans/{id}", 205).
+        then()
+                .log().all()
+                .statusCode(204);
+    }
+
+
+    @DisplayName("Testing DELETE")
+    @Test
+    public void testDelete(){
+
+        when()
+                .delete("/spartans/{id}", 199).
+         then()
+                .statusCode(204) ;
+               // .body( is(empty()));
+
+    }
+
+
+
+}
